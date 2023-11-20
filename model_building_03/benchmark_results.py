e8442381c93bf605eb52028f3ea8f3f1ff048b94
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2023-11-20 16:30:28 +0000

    merged files

--- model_building_03/benchmark_results.py
+++ model_building_03/benchmark_results.py
@@ -4,7 +4,8 @@ from scipy.stats import norm
 from sklearn.metrics import mean_pinball_loss
 import os
 
-os.chdir("/Users/emilryd/programming/water-supply-forecast")
+# os.chdir("/Users/emilryd/programming/water-supply-forecast")
+
 
 def calc_predictive_std(gt: pd.Series, preds: pd.Series) -> float:
     errs = gt - preds
@@ -16,17 +17,21 @@ def gen_predictive_quantile(preds: pd.Series, std: float, quantile: float) -> pd
     return preds + n_stds * std
 
 
-def main(gt_path: str, preds_path: str):
+def main(gt_path: str, preds_path: str, gen_std: bool = True):
     ground_truth = pd.read_csv(gt_path)["gt"]
     preds = pd.read_csv(preds_path)["pred"]
 
-    std = calc_predictive_std(ground_truth, preds)
     quantiles = [0.1, 0.5, 0.9]
-    quantile_preds = {quantile: gen_predictive_quantile(preds, std, quantile) for quantile in quantiles}
+    if gen_std:
+        std = calc_predictive_std(ground_truth, preds)
+        quantile_preds = {quantile: gen_predictive_quantile(preds, std, quantile) for quantile in quantiles}
+    else:
+        quantile_preds = {i: preds[f"{i}"].to_numpy() for i in quantiles}
+
     quantile_losses = {quantile: mean_pinball_loss(ground_truth, q_preds) for quantile, q_preds in
                        quantile_preds.items()}
     interval = (quantile_preds[0.1] <= ground_truth) & (ground_truth <= quantile_preds[0.9])
-    
+
     print(f'Mean quantile loss:{sum(quantile_losses.values()) / len(quantiles)}, '
           f'percent in 0.1-0.9 quantiles:{100 * np.mean(interval):.2f}%')
 
@@ -34,5 +39,5 @@ def main(gt_path: str, preds_path: str):
 if __name__ == '__main__':
     site_ids = pd.read_csv("02-data-cleaning/site_ids.csv")["site_id"]
     for site_id in site_ids:
-        main(gt_path=f"03-model-building/model-outputs/ground_truth{site_id}.csv", preds_path=f"03-model-building/model-outputs/linear-model-training-optimization/predicted{site_id}.csv")
-
+        main(gt_path=f"03-model-building/model-outputs/ground_truth{site_id}.csv",
+             preds_path=f"03-model-building/model-outputs/linear-model-training-optimization/predicted{site_id}.csv")
