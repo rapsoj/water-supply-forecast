4d2746ec4cc3ba677309cca98db511520fb849a0
rapsoj <jessicakristenr@gmail.com>
2023-11-26 16:21:45 +0000

    Updated pipeline

--- pipeline/helper_functions/cleaning.py
+++ pipeline/helper_functions/cleaning.py
@@ -6,19 +6,7 @@ import calendar
 from datetime import datetime
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
 
-# Dictionary to map month abbreviations to numeric values
-month_to_num = {
-    'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4,
-    'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8,
-    'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12
-}
-
-# Dictionary to map month abbreviations to numeric values
-month_to_num_up = {
-    'JAN': 1, 'FEB': 2, 'MAR': 3, 'APR': 4,
-    'MAY': 5, 'JUN': 6, 'JUL': 7, 'AUG': 8,
-    'SEP': 9, 'OCT': 10, 'NOV': 11, 'DEC': 12
-}
+from . import dictionaries
 
 def import_mjo(current_dir) :
     # Import mjo dataset
--- pipeline/pre-processing.py
+++ pipeline/pre-processing.py
@@ -9,32 +9,24 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
 # import warnings
 # warnings.filterwarnings("ignore")
 
+# Import import helper functions
+from helper_functions import cleaning
+from helper_functions import scaling
+
 ## Import datasets
 current_dir = os.getcwd()
 
-# Import import functions from cleaning.py
-from cleaning import import_mjo
-from cleaning import import_nino
-from cleaning import import_oni
-from cleaning import import_pdo
-from cleaning import import_pna
-from cleaning import import_soi1
-from cleaning import import_soi2
-from cleaning import import_flow
-from cleaning import import_grace
-from cleaning import import_snotel
-
 # Importing steps
-df_mjo = import_mjo(current_dir)
-df_nino = import_nino(current_dir)
-df_oni = import_oni(current_dir)
-df_pdo = import_pdo(current_dir)
-df_pna = import_pna(current_dir)
-df_soi1 = import_soi1(current_dir)
-df_soi2 = import_soi2(current_dir)
-df_flow = import_flow(current_dir)
-df_grace = import_grace(current_dir)
-df_snotel = import_snotel(current_dir)
+df_mjo = cleaning.import_mjo(current_dir)
+df_nino = cleaning.import_nino(current_dir)
+df_oni = cleaning.import_oni(current_dir)
+df_pdo = cleaning.import_pdo(current_dir)
+df_pna = cleaning.import_pna(current_dir)
+df_soi1 = cleaning.import_soi1(current_dir)
+df_soi2 = cleaning.import_soi2(current_dir)
+df_flow = cleaning.import_flow(current_dir)
+df_grace = cleaning.import_grace(current_dir)
+df_snotel = cleaning.import_snotel(current_dir)
 
 ## Pre-merge cleaning steps
 
@@ -42,31 +34,19 @@ df_snotel = import_snotel(current_dir)
 # More serious cleaning follows in later stages.
 
 # Import dictionaries used to transform month variable to numeric
-import dictionaries
-
-# Import cleaning functions from cleaning.py
-from cleaning import clean_mjo
-from cleaning import clean_nino
-from cleaning import clean_oni
-from cleaning import clean_pdo
-from cleaning import clean_pna
-from cleaning import clean_soi1
-from cleaning import clean_soi2
-from cleaning import clean_flow
-from cleaning import clean_grace
-from cleaning import clean_snotel
+from helper_functions import dictionaries
 
 # Run functions
-df_mjo = clean_mjo(df_mjo)
-df_nino = clean_nino(df_nino)
-df_oni = clean_nino(df_oni)
-df_pdo = clean_nino(df_pdo)
-df_pna = clean_pna(df_pna)
-df_soi1 = clean_soi1(df_soi1)
-df_soi2 = clean_soi2(df_soi2)
-df_flow = clean_flow(df_flow)
-df_grace = clean_grace(df_grace)
-df_snotel = clean_snotel(df_snotel)
+df_mjo = cleaning.clean_mjo(df_mjo)
+df_nino = cleaning.clean_nino(df_nino)
+df_oni = cleaning.clean_nino(df_oni)
+df_pdo = cleaning.clean_nino(df_pdo)
+df_pna = cleaning.clean_pna(df_pna)
+df_soi1 = cleaning.clean_soi1(df_soi1)
+df_soi2 = cleaning.clean_soi2(df_soi2)
+df_flow = cleaning.clean_flow(df_flow)
+df_grace = cleaning.clean_grace(df_grace)
+df_snotel = cleaning.clean_snotel(df_snotel)
 
 ## Merge code
 data_frames = [df_mjo, df_nino, df_oni, df_pdo, df_pna, df_soi1, 
@@ -80,9 +60,5 @@ df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['year', 'month', 
 
 # Feature engineering from features.py
 
-# Scaling
-from scaling import preprocess_column
-from scaling import preprocess_dataframe
-
 # Perform preprocessing on columns except day, month, year
-trans_vars = preprocess_dataframe(df_merged)
+trans_vars = scaling.preprocess_dataframe(df_merged)
