0fdc3588cf1201667ea75398e319ccdc3f9f77e3
Emil Ryd <circlevaccess@gmail.com>
2023-12-03 14:55:49 +0000

    fixed forecast_years

--- preprocessing/pre_ml_processing.py
+++ preprocessing/pre_ml_processing.py
@@ -28,7 +28,6 @@ def process_features(df: pd.DataFrame, mjo_data: pd.DataFrame, nino_data: pd.Dat
     feat_dates = pd.date_range(start=start_date, end=end_date, freq=f'{N_DAYS_DELTA}D')
     # todo we're throwing away data that we have here, can/should we use it eg for training?
     feat_dates = feat_dates[(feat_dates.month < 4) | (feat_dates.month > 9)].to_series()
-    forecast_years = df.forecast_year
     site_feat_cols = set(df.columns) - ({'site_id', 'date', 'forecast_year', 'station'} | set(date_cols))
 
     # average over data from different stations in the same day, todo - deal with this properly by using lat/lon data or something groovier
@@ -61,7 +60,8 @@ def process_features(df: pd.DataFrame, mjo_data: pd.DataFrame, nino_data: pd.Dat
 
     site_df = interp_df.join(other_cols_df)
     site_df['date'] = feat_dates.reset_index(drop=True)
-    site_df['forecast_year'] = forecast_years.reset_index(drop=True)
+    site_df['forecast_year'] = feat_dates.apply(lambda x: x.year + (x.month > 4)).reset_index(drop=True)  # set value as +1 for all
+
 
     # todo make sure this is after the train/test split, don't want leakage
     assert not site_df.isna().any().any(), 'Error - we have nans!'
@@ -153,9 +153,6 @@ def ml_preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
     # Keeping only SNOTEL data
     data = data.drop(columns=global_mjo_cols + global_nino_cols + global_oni_cols + global_misc_cols)
 
-    # Prediction months and dates
-    prediction_dates = [dt.datetime(year, month, day) for year in data.year.unique() for month in range(4, 8) for day in
-                        range(1, 30, 7)]
 
     # Removing sites with no snotel data
     # todo process this data separately
