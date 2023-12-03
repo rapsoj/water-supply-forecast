689fdf861246deae6b5bb2ce3c1c502094a95c3d
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2023-12-03 18:56:37 +0000

    temporarily deal with sites with no gt data

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -8,7 +8,8 @@ from preprocessing.pre_ml_processing import ml_preprocess_data
 
 
 def run_pipeline(test_years: tuple = tuple(np.arange(2005, 2024, 2)),
-                 validation_years: tuple = tuple(np.arange(1984, 2023, 8)), load_from_cache: bool = False):
+                 validation_years: tuple = tuple(np.arange(1984, 2023, 8)), gt_col: str = 'volume',
+                 load_from_cache: bool = True):
     print('Loading data')
     # todo add output_csv paths to preprocessing, especially the ml preprocessing
     basic_preprocessed_df = get_processed_dataset(load_from_cache=load_from_cache)
@@ -32,17 +33,25 @@ def run_pipeline(test_years: tuple = tuple(np.arange(2005, 2024, 2)),
         drop_cols = list(set(non_feat_cols + train_site.columns[train_site.isna().any()].to_list()))
 
         train_site = train_site.drop(columns=drop_cols)
+        train_site_gt = train_gt[train_gt.site_id == site_id]
         val_site = val_features[val_features.site_id == site_id].drop(columns=drop_cols)
+        val_site_gt = val_gt[val_gt.site_id == site_id]
         test_site = test_features[test_features.site_id == site_id]
         test_dates = test_site.date
         test_site = test_site.drop(columns=drop_cols, errors='ignore')
 
-        train_pred, val_pred, test_pred = gen_basin_preds(train_site, train_gt, val_site, val_gt, test_site)
+        # todo deal with this using a global model
+        if train_site_gt.empty or val_site_gt.empty:
+            print(f'No ground truth data for site {site_id}')
+            continue
+
+        train_pred, val_pred, test_pred = gen_basin_preds(train_site, train_site_gt[gt_col], val_site,
+                                                          val_site_gt[gt_col], test_site)
 
         results_id = f'{site_id}'
         print(f'Benchmarking results for site {site_id}')
-        train_pred, val_pred, test_pred = benchmark_results(train_pred, train_gt, val_pred, val_gt, test_pred,
-                                                            benchmark_id=results_id)
+        train_pred, val_pred, test_pred = benchmark_results(train_pred, train_site_gt[gt_col], val_pred,
+                                                            val_site_gt[gt_col], test_pred, benchmark_id=results_id)
         cache_preds(pred=test_pred, cache_id=results_id, site_id=site_id, pred_dates=test_dates)
 
 
