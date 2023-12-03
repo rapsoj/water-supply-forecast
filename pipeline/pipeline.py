60abef640e14d52ae80dd392bed698365ffe3c5b
Emil Ryd <circlevaccess@gmail.com>
2023-12-03 11:10:26 +0000

    small fix in test set

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -17,29 +17,27 @@ def run_pipeline(gt_col: str = 'gt', test_years: list = list(np.arange(2003, 202
     # Get training, validation and test sets
     train, val, test = train_val_test_split(processed_data, test_years, validation_years)
 
-    for test_year in test_years:
-        train, val, test = train_val_test_split(processed_data, test_year, )
-        assert gt_col not in test.columns, 'Error - test should not have a ground truth!'
+    assert gt_col not in test.columns, 'Error - test should not have a ground truth!'
 
-        # todo implement global models
-        site_ids = NotImplemented
-        for site_id in site_ids:
-            train_site = train[train.site_id == site_id]
-            val_site = val[val.site_id == site_id]
-            test_site = test[test.site_id == site_id]
+    # todo implement global models
+    site_ids = NotImplemented
+    for site_id in site_ids:
+        train_site = train[train.site_id == site_id]
+        val_site = val[val.site_id == site_id]
+        test_site = test[test.site_id == site_id]
 
-            train_gt = train_site.gt
-            train_site = train_site.drop(columns=gt_col)
-            val_gt = val_site.gt
-            val_site = val_site.drop(columns=gt_col)
+        train_gt = train_site.gt
+        train_site = train_site.drop(columns=gt_col)
+        val_gt = val_site.gt
+        val_site = val_site.drop(columns=gt_col)
 
-            train_pred, val_pred, test_pred = gen_basin_preds(train_site, val_site, test_site)
+        train_pred, val_pred, test_pred = gen_basin_preds(train_site, val_site, test_site)
 
-            results_id = f'{test_year}_{site_id}'
-            train_pred, val_pred, test_pred = benchmark_results(train_pred, train_gt, val_pred, val_gt, test_pred,
-                                                                benchmark_id=results_id)
-            # todo make sure that `dates` is the dates column
-            cache_preds(pred=test_pred, cache_id=results_id, site_id=site_id, pred_dates=test_site.dates)
+        results_id = f'{test_year}_{site_id}'
+        train_pred, val_pred, test_pred = benchmark_results(train_pred, train_gt, val_pred, val_gt, test_pred,
+                                                            benchmark_id=results_id)
+        # todo make sure that `dates` is the dates column
+        cache_preds(pred=test_pred, cache_id=results_id, site_id=site_id, pred_dates=test_site.dates)
 
 
 if __name__ == '__main__':
