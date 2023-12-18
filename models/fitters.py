1881735695606e638ee9022731b575e1398b2db1
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2023-12-18 23:55:44 +0000

    Revert "hacked min label clipping"
    This reverts commit 5edfe15fb01e1d3ff1842a4048c0cab68d968a50.

--- models/fitters.py
+++ models/fitters.py
@@ -18,6 +18,8 @@ from consts import DEF_QUANTILES, JULY, N_PREDS
 from models.lstm_utils import features2seqs, pad_collate_fn, train_lstm, DEF_LSTM_HYPPARAMS
 
 
+
+
 def base_feature_adapter(X, pca=None):
     X = (X[X.date.dt.month <= JULY].drop(columns=['date', 'forecast_year']))
     return X
@@ -84,7 +86,7 @@ class StreamflowModel:
         return loss
 
 
-def lstm_fitter(X, y, val_X, val_y, quantile: bool = True, min_label: float = -np.inf, **kwargs):
+def lstm_fitter(X, y, val_X, val_y, quantile: bool = True):
     assert quantile
 
     train_set = features2seqs(X, y)
@@ -100,11 +102,11 @@ def lstm_fitter(X, y, val_X, val_y, quantile: bool = True, min_label: float = -n
     train_model = LSTMModel(input_size=n_feats)
     full_model = LSTMModel(input_size=n_feats)
 
-    train_model = train_lstm(train_dloader, val_set, train_model, DEF_LSTM_HYPPARAMS.lr, DEF_LSTM_HYPPARAMS.n_epochs,
-                             min_label=min_label)
+
+
+    train_model = train_lstm(train_dloader, val_set, train_model, DEF_LSTM_HYPPARAMS.lr, DEF_LSTM_HYPPARAMS.n_epochs)
     # todo try fine-tuning trained model? probably no reason to do that, difficult to validate
-    full_model = train_lstm(full_dloader, None, full_model, DEF_LSTM_HYPPARAMS.lr, DEF_LSTM_HYPPARAMS.n_epochs,
-                            min_label=min_label)
+    full_model = train_lstm(full_dloader, None, full_model, DEF_LSTM_HYPPARAMS.lr, DEF_LSTM_HYPPARAMS.n_epochs)
 
     def lstm_feat_adapter(X):
         dataset = features2seqs(X)
