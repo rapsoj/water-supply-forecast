83d538b9cdfd820b31565d0136448142908bc243
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2023-12-18 18:12:47 +0000

    implemeneted sitewise empirical quantiles+regen data

--- models/lstm_playground.py
+++ models/lstm_playground.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader
 
 from consts import DEF_QUANTILES
 from models.fitters import LSTMModel
-from models.lstm_utils import train_lstm, pad_collate_fn, features2seqs, calc_val_loss, HypParams
+from models.lstm_utils import train_lstm, pad_collate_fn, features2seqs, calc_val_loss, HypParams, DEF_LSTM_HYPPARAMS
 
 
 def main():
@@ -20,18 +20,21 @@ def main():
     # X, y = data['train']
     # val_X, val_y = data['val']
 
+    # todo use regular standardisation, less copying
     X, val_X, test_X, y, val_y = data
     y_mean, y_std = y.volume.mean(), y.volume.std()
     y.volume = (y.volume - y_mean) / y_std
     val_y.volume = (val_y.volume - y_mean) / y_std
 
-    y_vol = y.volume
-
-    train_emp_aqm = np.mean([mean_pinball_loss(y_vol, [y_vol.quantile(q)] * len(y), alpha=q) for q in DEF_QUANTILES])
-    val_emp_aqm = np.mean([mean_pinball_loss(val_y.volume, [y_vol.quantile(q)] * len(val_y), alpha=q)
-                           for q in DEF_QUANTILES])
-    print(f"Empirical quantile's training loss: {train_emp_aqm:.3f}")
-    print(f"Empirical quantile's validation loss: {val_emp_aqm:.3f}")
+    train_emp_aqm = y.groupby('site_id').volume \
+        .apply(lambda x: np.mean([mean_pinball_loss(x, [x.quantile(q)] * len(x), alpha=q) for q in DEF_QUANTILES])) \
+        .mean()
+    val_emp_aqm = y.groupby('site_id').volume \
+        .apply(lambda x: np.mean([mean_pinball_loss(x, [y.volume[y.site_id == x.name].quantile(q)] * len(x), alpha=q)
+                                  for q in DEF_QUANTILES])) \
+        .mean()
+    print(f"Sitewise empirical quantile's training loss: {train_emp_aqm:.3f}")
+    print(f"Sitewise empirical quantile's validation loss: {val_emp_aqm:.3f}")
 
     train_set = features2seqs(X, y)
     val_set = features2seqs(val_X, val_y)
@@ -51,7 +54,7 @@ def main():
     shuffle(hyp_params_combs)
 
     results = []
-    for hyp_params in hyp_params_combs:
+    for hyp_params in [DEF_LSTM_HYPPARAMS]:
         if hyp_params.n_hidden == 1 and hyp_params.dropout_prob != min(DROPOUT_PROBS):
             continue  # arbitrary dropout prob, prevent recurring computations
 
@@ -66,12 +69,12 @@ def main():
 
         res_str = f'val loss: {val_loss:.5f}, {hyp_params}\n'
         # append new results to running text file
-        with open('lstm_results.txt', 'a+') as f:
-            f.write(res_str)
+        # with open('lstm_results.txt', 'a+') as f:
+        #     f.write(res_str)
 
         print(res_str)
 
-    pd.DataFrame.from_records(results).to_csv('lstm_results.csv')
+    # pd.DataFrame.from_records(results).to_csv('lstm_results.csv')
 
 
 if __name__ == '__main__':
