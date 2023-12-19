bdb65bfabe7856c176aeec62e8c567534cadae83
Emil Ryd <circlevaccess@gmail.com>
2023-12-19 16:27:36 +0100

    ground truth created (nans made to 0s, should be improved)

--- assets/generate_additional_site_ground_truth.py
+++ assets/generate_additional_site_ground_truth.py
@@ -7,15 +7,22 @@ monthly_flow = pd.read_csv(os.path.join(path, '..', 'assets', 'data', 'additiona
 metadata = pd.read_csv(os.path.join(path, '..', 'assets', 'data', 'additional_sites', 'metadata.csv'))
 
 monthly_flow = monthly_flow[(monthly_flow.month <= JULY) & (monthly_flow.month >= APRIL)]
+
+def process_zeros(df: pd.DataFrame) -> pd.DataFrame:
+    # todo fill nans with sitewise monthwise average
+    return df
+
+monthly_flow = monthly_flow.apply(process_zeros)
 monthly_flow.volume = monthly_flow.volume.fillna(0)
 
-gt = monthly_flow.groupby(['nrcs_id', 'year'], as_index=False).sum()
-gt.forecast_year = (gt.forecast_year/NUM_MONTHS_IN_SEASON).astype(int)
-gt.month = (gt.month/NUM_MONTHS_IN_SEASON).astype(int)
-gt = gt[gt.year >= FIRST_FULL_GT_YEAR]
+gt = monthly_flow.groupby(['nrcs_id', 'year'], as_index=False).volume.sum()
 
 site_ids = [metadata.nrcs_name[metadata.nrcs_id==x].iloc[0] for x in gt.nrcs_id]
+
 gt['site_id'] = pd.Series(data=site_ids)
+gt = gt[gt.year >= FIRST_FULL_GT_YEAR]
+gt = gt[gt.volume != 0]
+
 gt.site_id = gt.site_id.str.lower() \
     .replace('[ ]', '_', regex=True)
 
