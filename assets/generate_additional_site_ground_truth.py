1cb3114d39e1c99512ebd44c6163092cbde83721
Emil Ryd <circlevaccess@gmail.com>
2023-12-19 16:05:33 +0100

    hydrobasin summary csv file added to git

--- assets/generate_additional_site_ground_truth.py
+++ assets/generate_additional_site_ground_truth.py
@@ -1,9 +1,24 @@
 import os
 import pandas as pd
-
+from consts import JULY, APRIL, FIRST_FULL_GT_YEAR, NUM_MONTHS_IN_SEASON
 path = os.getcwd()
 
-monthly_flow = pd.read_csv(os.path.join(path, '..', 'data', 'additional_sites', 'train_monthly_naturalized_flow'))
-monthly_flow['site_id'] = monthly
+monthly_flow = pd.read_csv(os.path.join(path, '..', 'assets', 'data', 'additional_sites', 'train_monthly_naturalized_flow.csv'))
+metadata = pd.read_csv(os.path.join(path, '..', 'assets', 'data', 'additional_sites', 'metadata.csv'))
 
-gt = monthly_flow.groupby()
+monthly_flow = monthly_flow[(monthly_flow.month <= JULY) & (monthly_flow.month >= APRIL)]
+monthly_flow.volume = monthly_flow.volume.fillna(0)
+
+gt = monthly_flow.groupby(['nrcs_id', 'year'], as_index=False).sum()
+gt.forecast_year = (gt.forecast_year/NUM_MONTHS_IN_SEASON).astype(int)
+gt.month = (gt.month/NUM_MONTHS_IN_SEASON).astype(int)
+gt = gt[gt.year >= FIRST_FULL_GT_YEAR]
+
+site_ids = [metadata.nrcs_name[metadata.nrcs_id==x].iloc[0] for x in gt.nrcs_id]
+gt['site_id'] = pd.Series(data=site_ids)
+gt.site_id = gt.site_id.str.lower() \
+    .replace('[ ]', '_', regex=True)
+
+gt = gt.drop(columns='nrcs_id')
+gt.to_csv(os.path.join(path, '..', 'assets', 'data', 'additional_train.csv'))
+
