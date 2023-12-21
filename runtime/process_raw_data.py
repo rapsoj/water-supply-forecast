ee7189b3d5bea422903c2f1a22e3ff9ba0117010
Emil Ryd <circlevaccess@gmail.com>
2023-12-21 13:37:52 +0100

    WIP on process raw data

--- runtime/process_raw_data.py
+++ runtime/process_raw_data.py
@@ -1,7 +1,19 @@
 import pandas as pd
 
 
-def features_from_raw_data(raw_data: pd.DataFrame, ) -> pd.DataFrame:
+def features_from_raw_data(raw_data: pd.DataFrame, fill_nan_means: pd.DataFrame, scaling_means: pd.DataFrame,
+                           scaling_std: pd.DataFrame, group_cols: tuple) -> pd.DataFrame:
 
-    # df_merged, info: scaling dfs (fillna, mean and std)
+    # intput: df_merged, info: scaling dfs (fillna, mean and std), group_cols ('site_id, 'month')
+    # First, scale in accordance with scale_dataframe
+
+    scaled_data = scale_raw_data(raw_data, fill_nan_means, scaling_means, scaling_std, group_cols)
+
+def scale_raw_data(raw_data: pd.DataFrame, fill_nan_means: pd.DataFrame, scaling_means: pd.DataFrame,
+                           scaling_std: pd.DataFrame, group_cols: tuple) -> pd.DataFrame:
+
+    filled_data = raw_data.groupby(group_cols).fillna(fill_nan_means)
+
+    filled_data[non_fillna_cols] = filled_data[non_fillna_cols] * scaling_std + scaling_means
+
     raise NotImplementedError
