fad80bf056f288edb5dfc09f583c5534e59c30cc
Emil Ryd <circlevaccess@gmail.com>
2023-12-21 13:27:51 +0100

    drop cols

---
+++ runtime/process_raw_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+def features_from_raw_data(raw_data: pd.DataFrame, ) -> pd.DataFrame:
+
+    # df_merged, info: scaling dfs (fillna, mean and std)
+    raise NotImplementedError
